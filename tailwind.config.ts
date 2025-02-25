d7473fd12ba3e8e75905adac53fbb9402b690bc4
uchida189 <perukipoo@icloud.com>
2025-02-25 21:26:36 +0900

    feat:フォントサイズの設定を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,14 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        'headline-1': '2rem', // 32px
+        'headline-2': '1.5rem', // 24px
+        'body-1': '1rem', // 16px
+        'body-2': '0.875rem', // 14px
+        'label-1': '0.75rem', // 12px
+        'label-2': '0.6875rem', // 11px
+      },
     },
   },
   plugins: [],
