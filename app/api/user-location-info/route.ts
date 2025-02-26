86039fb0be3eb56b8861b79fd39868d1154c0549
YushinOgasawara <168957325+YushinOgasawara@users.noreply.github.com>
2025-02-26 09:20:17 +0900

    feat: ユーザー位置情報取得のためのGETリクエスト処理を追加

---
+++ app/api/user-location-info/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const GOOGLE_MAPS_GEOCODING_API_KEY = process.env.GOOGLE_MAPS_GEOCODING_API_KEY; // .envファイルにAPIキーを保存
+
+// GETリクエストの処理
+export async function GET(request: Request) {
+    if (!GOOGLE_MAPS_GEOCODING_API_KEY) {
+        return NextResponse.json({ error: 'API key is not set' }, { status: 500 });
+    }
+    const apiUrl = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + GOOGLE_MAPS_GEOCODING_API_KEY;
+
+    try {
+        // ユーザーの位置情報を取得
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({})
+        });
+
+        // エラーレスポンスの処理
+        if (!response.ok) {
+            return NextResponse.json({ error: 'Failed to get user location' }, { status: response.status });
+        }
+        
+        // レスポンスデータをJSON形式で返す
+        const data = await response.json();
+        return NextResponse.json(data);
+    }
+    // エラーハンドリング
+    catch (error) {
+        return NextResponse.json({ error: 'Server error', details: error }, { status: 500 });
+    }
+}
