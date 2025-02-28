bb91dfe17a45759606e0f6f1f3f840f25c476f07
uchida189 <perukipoo@icloud.com>
2025-02-28 11:29:12 +0900

    [feat]gitignoreに関連ファイルを追加

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development", // Disable PWA in development
 });
 
 const nextConfig = withPWA({
