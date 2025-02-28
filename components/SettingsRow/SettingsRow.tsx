3150f181d1f6cef71b0d38497e8b24d2d23a023f
uchida189 <perukipoo@icloud.com>
2025-02-28 19:11:31 +0900

    [feat]区切り線を追加

--- components/SettingsRow/SettingsRow.tsx
+++ components/SettingsRow/SettingsRow.tsx
@@ -12,7 +12,7 @@ interface SettingsRowProps {
 
 export default function SettingsRow(props: SettingsRowProps) {
   return (
-    <div className='w-full p-2 flex flex-col items-center justify-center'>
+    <div className='w-full py-2 flex flex-col items-center justify-center border-b-2 border-dashed border-white'>
       <SettingsQuestion question={props.question} />
       <div className='w-full flex justify-around'>
         {props.options.map((option) => (
