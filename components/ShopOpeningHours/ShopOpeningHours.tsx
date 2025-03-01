f30360535331e16dfd97f731fddcc9187f0293cc
kamijo-haruto <kamijoharuto@gmail.com>
2025-03-01 09:08:42 +0900

    [fix] 不正なコンテンツ配置を修正(<p>タグの中に<div>タグがあった)

--- components/ShopOpeningHours/ShopOpeningHours.tsx
+++ components/ShopOpeningHours/ShopOpeningHours.tsx
@@ -12,8 +12,8 @@ export default function ShopOpeningHours(props: ShopBudgetProps) {
     <div className="w-1/2 py-2 mx-auto flex justify-center items-center gap-4 border-2 border-main-3 rounded-xl">
       <IoTime color="#eea643"></IoTime>
         <div>
-            <p className='text-text-dark text-label-2 font-normal break-all flex justify-center items-center gap-6'><div>ランチ</div><div>{props.lunch}</div></p>
-            <p className='text-text-dark text-label-2 font-normal break-all flex justify-center items-center gap-3'><div>ディナー</div><div>{props.dinner}</div></p>
+            <div className='text-text-dark text-label-2 font-normal break-all flex justify-center items-center gap-6'><div>ランチ</div><div>{props.lunch}</div></div>
+            <div className='text-text-dark text-label-2 font-normal break-all flex justify-center items-center gap-3'><div>ディナー</div><div>{props.dinner}</div></div>
         </div>
     </div>
   );
